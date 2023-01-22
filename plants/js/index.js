4049a591d690c3895fd51df39bfabd5c67785979
ulianamakarovaulianamakarovaexitgit config --globalgit config --global user.name ulianamakarova <ulianamakarova@gmail.com>
2023-01-22 19:00:41 +0300

    fix: rebuild js with window on load

--- plants/js/index.js
+++ plants/js/index.js
@@ -1,6 +1,8 @@
+window.onload = function(){
 console.log('Максимальное количество баллов: 75\n');
 console.log('Невыполненные пункты:');
 console.log('Не успела к дедлайну выполнить адаптивное меню');
+}
 
 
 function menu_render() {
