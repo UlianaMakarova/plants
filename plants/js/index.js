8ed0a41bb44eefec2df728410e2c81fe60a70311
Uliana <uliana553663@gmial.com>
2023-01-23 14:54:33 +0300

    feat: build burger menu

--- plants/js/index.js
+++ plants/js/index.js
@@ -5,11 +5,39 @@ console.log('Не успела к дедлайну выполнить адапт
 }
 
 
-function menu_render() {
-    var x = document.getElementsByClassName("nav");
-    if (x.className === "nav") {
-        x.className += " hamburger";
-    } else {
-        x.className = "nav";
+//Меню бургер открыть
+const iconMenu = document.querySelector('.hamburger');
+const menuBody = document.querySelector('.hamburger-nav');
+
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+
+const menuLinks = document.querySelectorAll('.hamburger-nav-link');
+    // Для каждого элемента меню при клике вызываем ф-ию
+    menuLinks.forEach(menuLinks => {
+      menuLinks.addEventListener("click", closeOnClick);
+    });
+    
+    // Закрытие попапа при клике на меню
+    function closeOnClick() {
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+    }
+    const main = document.querySelector('main');
+    const footer = document.querySelector('footer');
+    //Закрытие меню при клике вне меню
+    main.addEventListener('click', outsideEvtListener);
+    footer.addEventListener('click', outsideEvtListener);
+
+    function outsideEvtListener(evt) {
+    if (evt.target === menuBody || menuBody.contains(evt.target)) {
+        // клик внутри
+        return;
+    }
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
     }
 }
